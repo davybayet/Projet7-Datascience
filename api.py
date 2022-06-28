7e68bc2fed0f70e2a5bc7461d94268c5860ef0d6
davybayet <82160438+davybayet@users.noreply.github.com>
2022-06-28 05:19:18 +0200

    Update api.py

--- api.py
+++ api.py
@@ -11,7 +11,8 @@ FILE_TEST_SET = 'resources/data/test_set.pickle'
 with open(FILE_TEST_SET, 'rb') as df_test_set:
             test_set = pickle.load(df_test_set)
 FILE_BEST_MODELE = 'resources/modele/best_model.pickle'
-best_model = pickle.load(model_lgbm)
+with open(FILE_BEST_MODELE, 'rb') as model_lgbm:
+            best_model = pickle.load(model_lgbm)
 
 print("API ready")
 
