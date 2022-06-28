a172a9ef198c849212e2664ca5eda18f52772bb6
davybayet <davy.bayetpro@gmail.com>
2022-06-28 10:07:38 +0200

    uploaded files

--- dashboard.py
+++ dashboard.py
@@ -284,10 +284,10 @@ st.markdown(html_score, unsafe_allow_html=True)
 
 # ============== Score du client en pourcentage ==> en utilisant le modèle ======================
 # Sélection des variables du clients
-X_test = test_set[test_set['SK_ID_CURR'] == client_id]
+# X_test = test_set[test_set['SK_ID_CURR'] == client_id]
 # # Score des prédictions de probabiltés
-y_proba = best_model.predict_proba(X_test.drop('SK_ID_CURR', axis=1))[:, 1]
-# y_proba = requests.get(f"172.21.119.146:8501/predict?clien_id={client_id}")
+# y_proba = best_model.predict_proba(X_test.drop('SK_ID_CURR', axis=1))[:, 1]
+y_proba = requests.get(f"https://myscoringdashboard.herokuapp.com/predict?clien_id={client_id}")
 # Score du client en pourcentage arrondi et nombre entier
 score_client = int(np.rint(y_proba * 100))
 
